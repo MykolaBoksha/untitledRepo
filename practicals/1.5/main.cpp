97a8ee162a568feaa75f552ca015d3aa251cc5f3
MykolaBoksha <9100477@stud.nau.edu.ua>
2024-10-27 22:19:41 +0200

    1.5prac

--- practicals/1.5/main.cpp
+++ practicals/1.5/main.cpp
@@ -1,53 +1,67 @@
-/**
- * Done by:
- * Student Name: Severyn Kotyhoroshko
- * Student Group: 123
- * Lab 1.5
- */
+// prac1.5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//Mykola Boksha
 
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-
-using namespace std;
-
-void initRandomizer() {
-    // Seed the random number generator with the current time
-    srand(time(0));  // srand(time(NULL)) could also be used
-}
-
-int main() {
-    // Задано послідовність значень А[n]. Знайти мінімальне значення у послідовності.
+int main()
+{
+ 
+    //ex 1
+    int A1[] = { 0, 18, 24, 7, 15, 2, 52, -3 };
+    int n1 = sizeof(A1) / sizeof(int);
+    int P1;
+    std::cout << "Enter P value: " << std::endl;
+    std::cin >> P1;
+    for (int i = 0; i < n1; i++)
     {
-        initRandomizer();
-
-        int n;
-        cout << "Enter the length of the sequence: ";
-        cin >> n;
-
-        std::vector<int> myVector(n);
-        // Populate the array with random values
-        for (int i = 0; i < n; i++) {
-            // arr[i] = rand();
-            int randomVar = (rand() % 100) - 50;
-            myVector[i] = randomVar;
+        if (A1[i] == P1)
+        {
+            std::cout << "Index of first appearence of P value in array is: " << i << std::endl;
         }
-
-        // cout the array
-        for (int i = 0; i < n; i++) {
-            cout << myVector[i] << " ";
+        else if (i == n1 - 1)
+        {
+            std::cout << "P value do not appear in array " << std::endl;
         }
-        cout << endl;
-                       // indexes: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-        int min = myVector[0]; // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        for (int i = 1; i < n; i++) {
-            if (myVector[i] < min) {
-                min = myVector[i];
-            }
-        }
-
-        cout << "The minimum value in the sequence is: " << min << endl;
     }
 
+    //ex 2
+
+    int A2[] = { 5, 4, 11, 22, 42, -42, -5, 0 };
+    int n2 = sizeof(A2) / sizeof(int);
+    int A2min = A2[0];
+    for (int i = 0; i < n2; i++)
+    {
+        if ((A2[i] < A2min) && (A2[i] >= 0))
+        {
+            A2min = A2[i];
+        }
+    }
+    std::cout << "The answer to task 2 is: " << A2min << std::endl;
+
+    //ex 3
+
+    int A3[] = { 4, 12, 42, -3, 54, 987, 11, -4 };
+    int n3 = sizeof(A3) / sizeof(int);
+    int A3min = A3[0];
+    int A3max = A3[0];
+    int MaxValueIndex, MinValueIndex = 0;
+    for (int i = 0; i < n3; i++)
+    {
+        if (A3[i] < A3min)
+        {
+            A3min = A3[i];
+            MinValueIndex = i;
+        }
+        else if (A3[i] > A3max)
+        {
+            A3max = A3[i];
+            MaxValueIndex = i;
+        }
+    }
+    int temp = A3[MaxValueIndex];
+    A3[MaxValueIndex] = A3[MinValueIndex];
+    A3[MinValueIndex] = temp;
+    std::cout << "Values are succesfully changed their places" << std::endl;
+
     return 0;
+}
 }
